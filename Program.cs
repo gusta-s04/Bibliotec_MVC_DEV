f66ce22d69d206187f6446bef8596a84fca5f828
Gustavo <gustavo.s.oliviera66@aluno.senai.br>
2026-05-25 11:15:14 -0300

    Atualização Bibliotec_Dev

--- Program.cs
+++ Program.cs
@@ -1,20 +1,33 @@
+using Bibliotec_MVC_DEV.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BbDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnecton")));
+
+    builder.Services.AddSession(options => {
+        options.IdleTimeout = TimeSpan.FromMinutes(30);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+    });
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
